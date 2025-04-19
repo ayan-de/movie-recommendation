6b6afdc30d92875090fca3e026bddf94c5ca8b6e
Ayan De <deayan252@gmail.com>
2025-04-20 05:04:44 +0530

    "framer motion refactoring"

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -7,11 +7,13 @@ import classNames from "classnames";
 type CardProps = {
   imgUrl: string;
   size: string;
+  id: string;
 };
 const Card = (props: CardProps) => {
   const {
     imgUrl = "https://images.unsplash.com/photo-1604975701397-6365ccbd028a?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     size = "medium",
+    id = "1",
   } = props;
 
   const [imgSrc, setImgSrc] = useState(imgUrl);
@@ -28,11 +30,13 @@ const Card = (props: CardProps) => {
     );
   };
 
+  const scale = id === "0" ? { scaleY: 1.1 } : { scale: 1.1 };
+
   return (
     <div className={styles.container}>
       <motion.div
         className={classNames(styles.imgMotionWrapper, classMap[size])}
-        whileHover={{ scale: 1.2 }}
+        whileHover={{ ...scale }}
       >
         <Image
           src={imgSrc}
--- components/card/section-card.tsx
+++ components/card/section-card.tsx
@@ -10,12 +10,12 @@ const SectionCards = (props: SectionProps) => {
     <section className={styles.container}>
       <h2 className={styles.title}>{title}</h2>
       <div className={styles.cardWrapper}>
-        <Card imgUrl="/static/animal.jpg" size="large" />
-        <Card imgUrl="/static/animal.jpg" size="large" />
-        <Card imgUrl="/static/animal.jpg" size="large" />
-        <Card imgUrl="/static/animal.jpg" size="large" />
-        <Card imgUrl="/static/animal.jpg" size="large" />
-        <Card imgUrl="/static/animal.jpg" size="large" />
+        <Card id="0" imgUrl="/static/animal.jpg" size="large" />
+        <Card id="1" imgUrl="/static/animal.jpg" size="large" />
+        <Card id="2" imgUrl="/static/animal.jpg" size="large" />
+        <Card id="3" imgUrl="/static/animal.jpg" size="large" />
+        <Card id="4" imgUrl="/static/animal.jpg" size="large" />
+        <Card id="5" imgUrl="/static/animal.jpg" size="large" />
       </div>
     </section>
   );
