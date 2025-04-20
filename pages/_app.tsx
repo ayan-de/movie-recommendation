f65b5b4cc32fe490a5ced32f939c0a381044a0c8
Ayan De <deayan252@gmail.com>
2025-04-21 01:44:41 +0530

    "loading flicker added"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import "@/styles/globals.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { magic } from "@/lib/magic-client";
 import type { AppProps } from "next/app";
 import { Roboto_Slab } from "next/font/google";
+import Loading from "../components/loading/Loading";
 
 const roboto = Roboto_Slab({
   weight: ["400", "700"],
@@ -12,6 +13,7 @@ const roboto = Roboto_Slab({
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const handleLoggedIn = async () => {
@@ -26,7 +28,22 @@ export default function App({ Component, pageProps }: AppProps) {
     };
     handleLoggedIn();
   }, []);
-  return (
+
+  useEffect(() => {
+    const handleComplete = () => {
+      setIsLoading(false);
+    };
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [router]);
+  return isLoading ? (
+    <Loading />
+  ) : (
     <main className={roboto.className}>
       <Component {...pageProps} />
     </main>
