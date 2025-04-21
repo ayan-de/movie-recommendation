461f1200f54195c8eb14f2687d1ef02b18d68ad4
Ayan De <deayan252@gmail.com>
2025-04-21 06:44:13 +0530

    "Model description styles added"

--- pages/video/[videoId].tsx
+++ pages/video/[videoId].tsx
@@ -9,6 +9,16 @@ const Video = () => {
   const router = useRouter();
   console.log({ router });
 
+  const video = {
+    title: "Hi cute dog",
+    publishTime: "1990-01-01",
+    description: "A big red dog that is super cute, can he get any bigger?",
+    channelTitle: "Paramount Pictures",
+    viewCount: 10000,
+  };
+
+  const { title, publishTime, description, channelTitle, viewCount } = video;
+
   return (
     <div className={styles.container}>
       {/* video page {router.query.videoId} */}
@@ -29,6 +39,27 @@ const Video = () => {
           src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
           frameBorder="0"
         ></iframe>
+
+        {/* video description */}
+        <div className={styles.modalBody}>
+          <div className={styles.modalBodyContent}>
+            <div className={styles.col1}>
+              <p className={styles.publishTime}>{publishTime}</p>
+              <p className={styles.title}>{title}</p>
+              <p className={styles.description}>{description}</p>
+            </div>
+            <div className={styles.col2}>
+              <p className={classNames(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>Cast: </span>
+                <span className={styles.channelTitle}>{channelTitle}</span>
+              </p>
+              <p className={classNames(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>View Count: </span>
+                <span className={styles.channelTitle}>{viewCount}</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </Modal>
     </div>
   );
